6bbff93cb94b6d7530e0a80a14bb63abe7697b5c
abdibrokhim <abdibrokhim@gmail.com>
2025-02-18 13:08:42 +0500

    notlink is officially live on Product Hunt! Upvote git add .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,6 +30,14 @@ export default function Home() {
       <Analytics />
       <div className="py-8 max-w-xl w-full px-4 md:px-0">
         <h1 className="text-[24px] text-center">hi, i&apos;m abee. i build things and make content about it. known mostly for hackathons and meme coins...</h1>
+        <h2 className="mt-4 text-[18px] text-center"><a className='text-[var(--brand-faint)] bg-[var(--brand-default)] px-1' href='https://www.producthunt.com/products/notlink' target='_blank' rel="noreferrer">notl.ink</a> is officially live on Product Hunt!<br></br><a className='hover:text-[var(--gray-700)] underline' href='https://www.producthunt.com/products/notlink' target='_blank' rel="noreferrer">Upvote</a>, please!!</h2>
+        <iframe 
+          className='mt-8 border border-[var(--gray-700)] p-3 object-cover mx-auto items-center justify-center'
+          src="https://cards.producthunt.com/cards/products/992403"
+          width={400}
+          height={350}
+          allowFullScreen>
+        </iframe>
         <div className="flex flex-col gap-8 w-full max-w-xl mt-8">
           {stuff.map((item) => (
             <a 
