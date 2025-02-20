e4275a79e55eb037581c1f525dc0b147b310cdcd
abdibrokhim <abdibrokhim@gmail.com>
2025-02-20 21:27:33 +0500

    defaultTheme=light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system">
+        <ThemeProvider attribute="class" defaultTheme="light">
           {children}
         </ThemeProvider>
       </body>
